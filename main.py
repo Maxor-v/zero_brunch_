ee69d2de7fa1c67fc23ff5cc975364ad623091b1
Maxor-v <maxim.v.orlov@yandex.ru>
2025-03-31 15:20:23 +0300

    add new func calc

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def area_circle(r):
-    s = 3.14 * r ** 2
-    return s
-
-if __name__ == '__main__':
-    print_hi('Федор')
-    print(area_circle(55))
+def calc(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a/b)
