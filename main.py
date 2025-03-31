3a1f31af7e25b40494099d86ca91d5b6733fa0cf
Maxor-v <maxim.v.orlov@yandex.ru>
2025-03-31 15:27:00 +0300

    add new func hello

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def area_circle(r):
-    s = 3.14 * r ** 2
-    return s
-
-if __name__ == '__main__':
-    print_hi('Федор')
-    print(area_circle(55))
+def hello(name):
+    print(f"Hello {name}")
